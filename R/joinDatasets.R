3d273908373afd70b1349dc1e08d1b7f567f8d41
Robert Bischoff <bischrob@gmail.com>
2024-11-14 09:25:40 -0700

    changed variable names for API

--- R/joinDatasets.R
+++ R/joinDatasets.R
@@ -5,23 +5,23 @@
 #' The function requires a column named 'datasetID' in both datasets that has the CMID of the translated dataset. It also requires the columns used to create the 'Key' used in translation. For example, if the Key for the GADM dataset 'SD1' is 'GID: AFG', then the 'Key' column should be 'GID' in the dataset.
 #'
 #' @param database A string specifying the database to use, either "SocioMap" or "ArchaMap".
-#' @param autoLeft A data frame representing the left dataset with a "datasetID" column and other relevant columns.
-#' @param autoRight A data frame representing the right dataset with a "datasetID" column and other relevant columns.
+#' @param joinLeft A data frame representing the left dataset with a "datasetID" column and other relevant columns.
+#' @param joinRight A data frame representing the right dataset with a "datasetID" column and other relevant columns.
 #'
 #' @return A data frame containing the joined datasets or an error message if the request fails.
 #' @export
 #'
 #' @examples
-#' autoLeft = data.frame(datasetID = "SD1", country = "Afghanistan", GID = "AFG", val0 = 1)
-#' autoRight = data.frame(datasetID = "SD2", country = "Afghanistan", geonameid = "1149361", val1 = 2)
-#' joinDatasets("SocioMap", autoLeft, autoRight)
-joinDatasets <- function(database, autoLeft, autoRight) {
+#' joinLeft = data.frame(datasetID = "SD1", country = "Afghanistan", GID = "AFG", val0 = 1)
+#' joinRight = data.frame(datasetID = "SD2", country = "Afghanistan", geonameid = "1149361", val1 = 2)
+#' joinDatasets("SocioMap", joinLeft, joinRight)
+joinDatasets <- function(database, joinLeft, joinRight) {
 
   # Set up parameters for the API request
   parameters <- list(
     database = database,
-    autoLeft = autoLeft,
-    autoRight = autoRight
+    joinLeft = joinLeft,
+    joinRight = joinRight
   )
 
   # Call the API using callAPI function
